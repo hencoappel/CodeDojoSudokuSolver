d71be7079755a70f99a4e14c89032482dcd8e502
joe202m <j.g.metcalfe@gmail.com>
2014-09-11 20:29:57 +0100

    Add a sample grid

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -1,26 +1,71 @@
 $(document).ready(function() {
 	$('#view').innerhtml = "your mom";
-	var g = createEmptyGrid();
-	g[0][0] = 1;
+	var g = get_sample_grid();
 	display(g);
 });
 
 function check_valid(board, x, y, number) {
     for(i=0; i<9;i++ ){
-        if(board[x][i] === number || board[i][y] === number){
+        if(Math.abs(board[x][i]) === number || Math.abs(board[i][y]) === number){
             return false;
         }
         var box_x = x - (x % 3);
         var box_y = y - (y % 3);
         for (var i = 0; i < 3; i++) {
             for (var i = 0; i < 3; i++) {
-                if(board[box_x+i][box_y+j] == number) {
+                if(Math.abs(board[box_x+i][box_y+j]) == number) {
                     return false;
                 }
             }
         }
     }
     return true;
+}
+
+function get_sample_grid() {
+	var sample_grid = createEmptyGrid();
+	sample_grid[2][0] = -2;
+	sample_grid[3][0] = -6;
+	sample_grid[6][0] = -5;
+	sample_grid[7][0] = -9;
+	sample_grid[8][0] = -8;
+	sample_grid[0][1] = -4;
+	sample_grid[2][1] = -3;
+	sample_grid[4][1] = -8;
+	sample_grid[7][1] = -3;
+	sample_grid[0][2] = -5;
+	sample_grid[1][2] = -8;
+	sample_grid[3][2] = -9;
+	sample_grid[4][2] = -2;
+	sample_grid[5][2] = -7;
+	sample_grid[8][2] = -4;
+	sample_grid[2][3] = -9;
+	sample_grid[4][3] = -4;
+	sample_grid[1][4] = -4;
+	sample_grid[2][4] = -7;
+	sample_grid[3][4] = -2;
+	sample_grid[4][4] = -5;
+	sample_grid[6][4] = -8;
+	sample_grid[7][4] = -3;
+	sample_grid[8][4] = -1;
+	sample_grid[4][5] = -7;
+	sample_grid[6][5] = -6;
+	sample_grid[0][6] = -7;
+	sample_grid[3][6] = -4;
+	sample_grid[4][6] = -6;
+	sample_grid[5][6] = -2;
+	sample_grid[7][6] = -5;
+	sample_grid[8][6] = -3;
+	sample_grid[1][7] = -6;
+	sample_grid[4][7] = -1;
+	sample_grid[6][7] = -4;
+	sample_grid[8][7] = -2;
+	sample_grid[0][8] = -2;
+	sample_grid[1][8] = -3;
+	sample_grid[2][8] = -4;
+	sample_grid[5][8] = -5;
+	sample_grid[6][8] = -8;
+	return sample_grid;
 }
 
 function createEmptyGrid() {
@@ -46,7 +91,7 @@ function toString(grid) {
 	for (var y=0; y<9; y++) {
 		s += "<tr>"
 		for (var x=0; x<9; x++) {
-			s += "<td>" + grid[x][y] + "</td>";
+			s += "<td>" + Math.abs(grid[x][y]) + "</td>";
 		}
 		s += "<tr/>";
 	}
