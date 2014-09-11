a2bb05d11fbe8e6a25dcf4994c2cfe763388044c
joe202m <j.g.metcalfe@gmail.com>
2014-09-11 20:08:28 +0100

    check valid move

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 	$('#view').innerhtml = "your mom";
 });
+
+function check_valid(board, x, y, number) {
+    for(i=0; i<9;i++ ){
+        if(board[x][i] === number || board[i][y] === number){
+            return false;
+        }
+        var box_x = x - (x % 3);
+        var box_y = y - (y % 3);
+        for (var i = 0; i < 3; i++;) {
+            for (var i = 0; i < 3; i++;) {
+                if(board[box_x+i][box_y+j] == number) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
